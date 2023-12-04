6b866dee2ed76abdbde8b0fea7e15fb38d34e9d1
sandrarv94 <sandramilenariosv@gmail.com>
2023-12-04 15:39:57 -0500

    se inicia

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Entradas del problema
+        String nombreUsuario ="Daniel Puerta";
+        short edadUsuario=20;
+        String telefonoUsuario="3103568928";
+        String documentoIdentidad="1017233498";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario="carrera 74 #90-78";
+        double costoInscripcion=150000.0;
+        float tasaInteresNatillera=0.1f;
+        double saldoFinalUsuario;
+        // Proceso------------------------------------------------>
+        // 1. asignar un valor especifico a una variable desde el teclado
+        Scanner leerDato= new Scanner(System.in);
+        System.out.println("hola, aa");
+        leerDato.nextLine();
+        
+    }
+}
